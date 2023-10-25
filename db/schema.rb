1f1c43ea1a8437a9b2b69cee833830a9c1f3e220
Fatima Najafi <najafifatima762@gmail.com>
2023-10-25 07:19:27 +0430

    Create the user-migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_173456) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_24_201010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "author_id"
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_173456) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
 end
