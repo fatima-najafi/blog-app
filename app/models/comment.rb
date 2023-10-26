b568906437dc014b8e640cda7b1ae1e7bd90c57a
Fatima Najafi <najafifatima762@gmail.com>
2023-10-26 13:35:30 +0430

    Make the method private

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :update_post_comments_counter
+   
+  private
 
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
