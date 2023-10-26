33dc0add070fc29d6614ee1388782c656234439d
Fatima Najafi <najafifatima762@gmail.com>
2023-10-26 15:49:44 +0430

    Validate the title

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  after_save :update_user_posts_counter
+  validates :title, presence: true, length: {maximum: 250}
+  # validates :comments_counter, numericality: {only_integer:true, greater_than_or_equal_to: 0}
+  # validates :likes_counter, numericality :{only_integer:true, greater_than_or-equal_to: 0}
 
+  after_save :update_user_posts_counter
+  
   def recent_comments
     comments.order(created_at(Desc)).limit(5)
   end
