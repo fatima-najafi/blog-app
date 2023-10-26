1c7999c138bdfdd1afd156e6e550d3da46007e30
Fatima Najafi <najafifatima762@gmail.com>
2023-10-26 11:24:33 +0430

    Add the comment post method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User' , foreign_key: :author_id
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments
+  has_many :likes
+
+ after_save :update_user_posts_counter
+
+  def update_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+  
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
