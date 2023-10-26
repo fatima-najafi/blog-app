e980cc13d7bc2739a7af894cc99af6a06aa2ddbb
Fatima Najafi <najafifatima762@gmail.com>
2023-10-26 15:50:17 +0430

    Validate the likes_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: {maximum: 250}
   validates :comments_counter, numericality: {only_integer:true, greater_than_or_equal_to: 0}
-  # validates :likes_counter, numericality :{only_integer:true, greater_than_or-equal_to: 0}
+  validates :likes_counter, numericality :{only_integer:true, greater_than_or-equal_to: 0}
 
   after_save :update_user_posts_counter
   
