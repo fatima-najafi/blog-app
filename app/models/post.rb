45881c49072f49efc782573830667c816d61b8cd
Fatima Najafi <najafifatima762@gmail.com>
2023-10-26 12:00:42 +0430

    Update the user.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
- after_save :update_user_posts_counter
+  after_save :update_user_posts_counter
 
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
   end
-  
+
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
