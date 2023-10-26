1ba575aaedc03b1b51d4c44ad9beb326c9408d66
Fatima Najafi <najafifatima762@gmail.com>
2023-10-26 11:24:19 +0430

    Add the comment like method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :post
+
+     after_save :update_post_likes_counter
+  def update_post_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
