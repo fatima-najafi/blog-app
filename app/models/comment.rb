e15921df8d5ed9f7700f7d078b581bf72aeddf3d
Fatima Najafi <najafifatima762@gmail.com>
2023-10-26 11:49:04 +0430

    Fix lintes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,11 @@
 class Comment < ApplicationRecord
-   belongs_to :author, class_name: 'User'
-   belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-     after_save :update_post_comments_counter
-    
+  after_save :update_post_comments_counter
+
 
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
-  
   end
 end
