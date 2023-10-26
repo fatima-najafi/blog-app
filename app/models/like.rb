b4becd1434e203236587f2d0458fa1af26791438
Fatima Najafi <najafifatima762@gmail.com>
2023-10-26 11:56:14 +0430

    Upadate

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-     after_save :update_post_likes_counter
+  after_save :update_post_likes_counter
   def update_post_likes_counter
     post.update(likes_counter: post.likes.count)
   end
