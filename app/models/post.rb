3a218c4df45f7e8677404df215eb7a16ecdac34f
Fatima Najafi <najafifatima762@gmail.com>
2023-10-27 10:21:03 +0430

    Create validation for title

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  validates :title, presence: true, length: {maximum: 250}
-  validates :comments_counter, numericality: {only_integer:true, greater_than_or_equal_to: 0}
-  validates :likes_counter, numericality :{only_integer:true, greater_than_or-equal_to: 0}
+   validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_user_posts_counter
-  
-  def recent_comments
-    comments.order(created_at(Desc)).limit(5)
-  end
 
   private
 
@@ -19,5 +15,7 @@ class Post < ApplicationRecord
     author.update(posts_counter: author.posts.count)
   end
 
-
+  def recent_comments
+     comments.order(created_at: :desc).limit(5)
+  end
 end
