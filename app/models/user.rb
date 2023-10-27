2da5c1e86420c64139af199b16e239980bcc8e88
Fatima Najafi <najafifatima762@gmail.com>
2023-10-27 10:21:23 +0430

    Create validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-    validates :name, presence: true
-    validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :bio, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_posts
-    posts.order(created_at(Desc)).limit(3)
+       posts.order(created_at: :desc).limit(3)
   end
 end
