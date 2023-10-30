f958184df640e3e95c0482f4c44ac35d34cc610e
Fatima Najafi <najafifatima762@gmail.com>
2023-10-30 09:34:39 +0430

    Add code in index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    root "users#index"
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show]
   end
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
