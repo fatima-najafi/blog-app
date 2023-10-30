d15f607441072be6e6625a43286f42aa24f885ed
Fatima Najafi <najafifatima762@gmail.com>
2023-10-30 09:50:46 +0430

    Add index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
     def index
-        
+        @posts= Post.all
     end
 
     def show
-        
+        puts params
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
     def index
-        
+        @users= User.all
     end
 
     def show
-        
+        puts params
     end
 end
