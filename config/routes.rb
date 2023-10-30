6fab8e7f527cf91369cc19889ce213c2b86d6c07
Fatima Najafi <najafifatima762@gmail.com>
2023-10-30 07:35:34 +0430

    Create routes for users and postts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
