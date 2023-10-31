30d3be797e1d43532e57b8a7e633b77ba4f0216c
Fatima Najafi <najafifatima762@gmail.com>
2023-10-31 18:31:04 +0430

    Update the show methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
-  #index action displaying a list of posts belonging to a specific user
-  #first retrieves the user record base on user_id
-  #next fetches all posts associated with that user using @user.posts
+  # index action displaying a list of posts belonging to a specific user
+  # first retrieves the user record base on user_id
+  # next fetches all posts associated with that user using @user.posts
 
   def index
-    @user= User.find(params[:user_id])
-    @posts= @user.posts
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
-  @post= Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
