62f8f73a59f53aa2cebe2af9f910a36afcfad921
Fatima Najafi <najafifatima762@gmail.com>
2023-10-31 14:12:23 +0430

    Add the show action code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  #The index action is responsible for displaying a list of users. It fetches all user records from the database 
+  # assigns them to an instance variable @users.
+  # This instance variable can be accessed within the corresponding view file to display the list of users.
   def index
-    @users = User.all
+    @users = User.all   
   end
-
-  def show; end
+  # the show action retrieves a user record from the database based on the id parameter passed in the request (params[:id]).
+  def show
+  @user = User.find(params[:id])
+  end
 end
