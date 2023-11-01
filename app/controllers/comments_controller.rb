bcc5870a86c1c181bd275d7d3f23231dd09344dc
Fatima Najafi <najafifatima762@gmail.com>
2023-11-01 18:16:10 +0430

    Implement the create and new action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,24 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
 
   def create
+        @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment.author_id = current_user.id
+    @comment.post_id = @post.id
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @post.author_id, id: @post.id)
+    else
+      flash[:alert] = 'An error has occurred while creating the comment'
+      render :new
+    end
+  end
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
