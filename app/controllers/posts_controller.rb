e18dc170f829cd546214c3c22dd87dfb79c8c232
Fatima Najafi <najafifatima762@gmail.com>
2023-11-01 16:42:48 +0430

    Update the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to user_posts_path(id: current_user.id)
+    else
+      flash[:alert] = 'Cannot create a new post'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
