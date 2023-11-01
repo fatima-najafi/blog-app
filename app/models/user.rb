c1101ec010e2d643b24545a6b990020d57a677f2
Fatima Najafi <najafifatima762@gmail.com>
2023-11-01 15:51:11 +0430

    Update the user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :photo, presence: true
   validates :bio, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true,greater_than_or_equal_to: 0, allow_nil: true }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
