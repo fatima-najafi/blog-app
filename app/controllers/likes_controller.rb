0913d08322b40acb087a5d638fd3d47565e4cbff
Fatima Najafi <najafifatima762@gmail.com>
2023-11-01 18:18:04 +0430

    Implement the create action in likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,5 +4,13 @@ class LikesController < ApplicationController
   end
 
   def create
+     @post = Post.find(params[:post_id])
+    @like = Like.new(author_id: current_user.id, post_id: @post.id)
+    if @like.save
+      redirect_to user_post_path(user_id: @post.author_id, id: @post.id)
+    else
+      flash[:alert] = 'Can not add new like'
+      render :new
+    end
   end
 end
