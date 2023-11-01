4043cd5fd5c417eb1558b1111a0a990d409576d3
Fatima Najafi <najafifatima762@gmail.com>
2023-11-01 18:11:02 +0430

    Add likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'likes/new'
+  get 'likes/create'
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
